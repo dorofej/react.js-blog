725b36f99481f1c134356c48937e498594185854
Dorosh Andrey <dorosh.andrey@yahoo.com>
2018-10-25 15:00:46 +0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './App.less';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				React.js Blog
+			</div>
+		);
+	}
+};
+
+export default App;
