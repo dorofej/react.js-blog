dbf0beeeb16ee08ef47c68801b3ae87bd0bfa25b
Dorosh Andrey <dorosh.andrey@yahoo.com>
2018-10-27 13:12:20 +0300

    Create search input.

--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -1,40 +1,89 @@
 /* eslint-disable import/first */
 const l = require('utils/log')(module);
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import SearchInput, { createFilter } from 'react-search-input';
 
 import './styles.less';
 
 
 const shortStrLen = 110;
+const KEYS_TO_FILTERS = ['title', 'body'];
 
-const Posts = ({ posts }) => {
-	l();
+class Posts extends Component {
+	constructor(props) {
+		l();
 
-	return (
-		<div className="app__posts">
-			{
-				posts.map(({ title, body, id }) => (
-					<div className="app__post-container">
-						<span className="app__post-title">
-							{title}
-						</span>
-						<div className="app__post-body">
-							{ body.length < shortStrLen + 5 ? body : body.slice(0, shortStrLen) + '...'}
+		super(props);
+
+		this.handleSearch = this.handleSearch.bind(this);
+
+		this.state = {
+			searchTerm: '',
+		};
+	}
+
+	handleSearch(searchTerm) {
+		l();
+
+		this.setState({ searchTerm });
+	}
+
+	getFilteredPosts() {
+		l();
+
+		return this.props.posts
+			.filter(createFilter(
+				this.state.searchTerm,
+				KEYS_TO_FILTERS
+			));
+	}
+
+	renderSearchBox() {
+		l();
+
+		return (
+			<div className="app__posts-search-box">
+				<SearchInput
+					value={this.state.searchTerm}
+					className="search-box__input"
+					onChange={this.handleSearch}
+				/>
+			</div>
+		);
+	}
+
+	render() {
+		l();
+
+		const posts = this.getFilteredPosts();
+
+		return (
+			<div className="app__posts">
+				{this.renderSearchBox()}
+				{
+					posts.map(({ title, body, id }) => (
+						<div className="app__post-container">
+							<span className="app__post-title">
+								{title}
+							</span>
+							<div className="app__post-body">
+								{ body.length < shortStrLen + 5 ? body : body.slice(0, shortStrLen) + '...'}
+							</div>
+							<Link
+								to={`/posts/${id}`}
+								className="app__post-read-button"
+							>
+								READ
+							</Link>
 						</div>
-						<Link
-							to={`/posts/${id}`}
-							className="app__post-read-button"
-						>
-							READ
-						</Link>
-					</div>
-				))
-			}
-		</div>
-	);
+					))
+				}
+			</div>
+		);
+	}
 };
 
 const state2Props = ({ Posts }) => ({
