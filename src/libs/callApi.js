f7887e831bb70807f01b9b005438c03191d49b58
Dorosh Andrey <dorosh.andrey@yahoo.com>
2020-03-10 21:07:01 +0200

    Add axios

--- src/libs/callApi.js
+++ src/libs/callApi.js
@@ -1,27 +1,9 @@
+import axios from 'axios';
+
 const API_PREFIX = 'https://jsonplaceholder.typicode.com';
 
-export default function callApi(
-  endPoint,
-  method = 'GET',
-  body,
-  headers = { 'Content-Type': 'application/json' }
-) {
-
-  return fetch(`${API_PREFIX}/${endPoint}`, {
-    headers,
-    method,
-    body: JSON.stringify(body),
-  })
-    .then((response) => (response.json().then(json => ({ json, response }))))
-    .then(({ json, response }) => {
-      if (!response.ok) {
-        return Promise.reject(json);
-      };
-
-      return json;
-    })
-    .then(
-      (response) => { return response; },
-      (error) => { throw error; }
-    );
+export default async function callApi(endPoint, method = 'get', data) {
+  const url = `${API_PREFIX}/${endPoint}`;
+  const response = await axios({ method, url, data });
+  return response.data;
 };
