c1723d2a33288162b0675e56abec133392c5aa17
Dorosh Andrey <dorosh.andrey@yahoo.com>
2020-03-11 20:28:16 +0200

    Add JSDoc comment to callApi

--- src/libs/callApi.js
+++ src/libs/callApi.js
@@ -2,8 +2,14 @@ import axios from 'axios';
 
 const API_PREFIX = 'https://jsonplaceholder.typicode.com';
 
-export default async function callApi(endPoint, method = 'get', data) {
-  const url = `${API_PREFIX}/${endPoint}`;
+/**
+ * @param {string} route
+ * @param {string} method
+ * @param {*} data
+ * @return {any}
+ */
+export default async function callApi(route, method = 'get', data) {
+  const url = `${API_PREFIX}/${route}`;
   const response = await axios({ method, url, data });
   return response.data;
 };
